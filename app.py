27011c5a7aef634dd7d02b48b05da5dbb2113547
antonfokus <115660761+antonfokus@users.noreply.github.com>
2024-04-27 22:23:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -217,8 +217,8 @@ def main():
     st.subheader('Предсказание типа погоды')
     weathers = [st.selectbox(f'Погода в день #{i+1}',('drizzle', 'rain', 'sun', 'snow','fog')) for i in range(10)]
     if st.button("Предсказать погоду"):
-        coded_data = [weather_encoding[day] for day in weathers]
-        predicted_weather_index = np.argmax(weather_model.predict(coded_data))
+        coded_weather = [weather_encoding[j] for j in weathers]
+        predicted_weather_index = np.argmax(weather_model.predict(coded_weather))
         predicted_weather = list(weather_encoding.keys())[predicted_index]
         st.write(predicted_weather)
 
