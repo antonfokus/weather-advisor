524ac27221638fd15df201adf3b6e5453a7d13e8
antonfokus <115660761+antonfokus@users.noreply.github.com>
2024-05-22 23:45:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ weather_colors = {
 def main():
 
     st.image("assets/mascot.png", width=180)
-    st.title(":purple[Погодный Советник]")
+    st.title(:purple[Погодный Советник])
     st.header("Предскажет погоду, подскажет фильмы и песни!")
 
     
