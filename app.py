846a639d740ba36b2205813544ac2d23f3e89cb3
antonfokus <115660761+antonfokus@users.noreply.github.com>
2024-05-23 21:59:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ temp_model = keras.models.load_model("temp_full.keras")
 weather_model = keras.models.load_model("weather_full.keras")
 weather_encoding = {"drizzle": 0, "rain": 1, "sun": 2, "snow": 3, "fog": 4}
 weather_encoding_ru = {"морось": 0, "дождь": 1, "солнце": 2, "снег": 3, "туман": 4}
+weather_encoding_ru_text = {":violet[пасмурную:cloud:]": 0, ":blue[дождливую:rain_cloud:]": 1, ":orange[солнечную:sunny:]": 2, ":blue[снежную:snowflake:]": 3, ":gray[туманную:fog:]": 4}
 
 
 weather_images = {
@@ -71,26 +72,26 @@ def main():
         predicted_weather = list(weather_encoding.keys())[predicted_weather_index]
 
         st.subheader(f'Я думаю, что градусник покажет примерно {int(temp_result[0])}°C, а за окном будет {list(weather_encoding_ru.keys())[predicted_weather_index]}')
-        
+        st.subheader(f'А вот что можно посмотреть и послушать в {list(weather_encoding_ru_text.keys())[predicted_weather_index]} погоду')
         container = st.container(border=True)
         with container:
             st.subheader(f"Рекомендуемые фильмы")
             recommended_movies = get_movies_by_weather("Weather", "Description", predicted_weather, 20)
             col1, col2, col3 = st.columns(3)
             with col1:
-                st.write(recommended_movies.loc[0, "Title"])
+                st.write(f"**{recommended_movies.loc[0, "Title"]}**")
                 st.write(recommended_movies.loc[0, "Genre"])
                 st.write(recommended_movies.loc[0, "Rating"])
                 st.write(recommended_movies.loc[0, "Description"])
 
             with col2:
-                st.write(recommended_movies.loc[1, "Title"])
+                st.write(f"**{recommended_movies.loc[1, "Title"]}**")
                 st.write(recommended_movies.loc[1, "Genre"])
                 st.write(recommended_movies.loc[1, "Rating"])
                 st.write(recommended_movies.loc[1, "Description"])
 
             with col3:
-                st.write(recommended_movies.loc[2, "Title"])
+                st.write(f"**{recommended_movies.loc[2, "Title"]}**")
                 st.write(recommended_movies.loc[2, "Genre"])
                 st.write(recommended_movies.loc[2, "Rating"])
                 st.write(recommended_movies.loc[2, "Description"])
@@ -101,19 +102,19 @@ def main():
             # st.dataframe(recommended_songs)
             col1, col2, col3 = st.columns(3)
             with col1:
-                st.write(recommended_songs.loc[0, "Track Name"])
+                st.write(f"{recommended_songs.loc[0, "Track Name"]}")
+                st.write(recommended_songs.loc[0, "Artist"])
                 st.image(recommended_songs.loc[0, "Image"], use_column_width=True)
-                st.write(recommended_songs.loc[0, "Album"])
 
             with col2:
-                st.write(recommended_songs.loc[1, "Track Name"])
+                st.write(f"{recommended_songs.loc[1, "Track Name"]}")
+                st.write(recommended_songs.loc[1, "Artist"])
                 st.image(recommended_songs.loc[1, "Image"], use_column_width=True)
-                st.write(recommended_songs.loc[1, "Album"])
 
             with col3:
-                st.write(recommended_songs.loc[2, "Track Name"])
+                st.write(f"{recommended_songs.loc[2, "Track Name"]}")
+                st.write(recommended_songs.loc[2, "Artist"])
                 st.image(recommended_songs.loc[2, "Image"], use_column_width=True)
-                st.write(recommended_songs.loc[2, "Album"])
         
 
     
